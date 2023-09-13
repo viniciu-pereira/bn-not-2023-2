f45226241fe07117a52470d739d2317f2e7eb98d
vincius-pereira <viniciusfelipipereira@gmail.com>
2023-09-13 20:31:27 -0300

    13/09

--- back-end/src/routes/curso.js
+++ back-end/src/routes/curso.js
@@ -5,4 +5,12 @@ const router = Router()
 
 router.post('/', controller.create)
 
+router.get('/', controller.retrieveAll)
+
+router.get('/:id', controller.retrieveOne)
+
+router.put('/', controller.update)
+
+router.delete('/', controller.delete)
+
 export default router
